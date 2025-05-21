fc9cc6be34758593ca28287bbf27dec85585ee61
Brandon-Key <bkey@mindex.com>
2025-05-21 10:38:13 -0400

    Added extensions

---
+++ BKey.Utils.CSharpToTypeScript/Extensions/TypeScriptGenerationResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using BKey.Utils.CSharpToTypeScript.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKey.Utils.CSharpToTypeScript.Extensions;
+public static class TypeScriptGenerationResultExtensions
+{
+    public static string ToSingleFile(this TypeScriptGenerationResult result)
+    {
+        return string.Join("\n\n", result.Files.Select(x => x.Content));
+    }
+
+    public static TypeScriptGenerationResult Save(this TypeScriptGenerationResult result, string path = ".")
+    {
+        foreach (var file in result.Files)
+        {
+            using var stream = File.Create(Path.Join(path, file.FileName));
+            using var writer = new StreamWriter(stream);
+            writer.Write(file.Content);
+        }
+        return result;
+    }
+}
